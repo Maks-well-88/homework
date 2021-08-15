64dec29d9c9d1a9833209eb68d43e8e10eb7fb40
Maksim Pisartsev <Maxwell88@yandex.ru>
2021-08-15 14:51:25 +0300

    Refactoring actions for method hook_wagon

--- lesson_8/module_manage_wagons.rb
+++ lesson_8/module_manage_wagons.rb
@@ -5,24 +5,20 @@ module ManageWagons
     show_accessible_trains
     print 'Выберите поезд: '
     train = gets.chomp.to_i
-    trains[train].wagons.each_with_index do |wagon, index|
-      puts "Вагон, индекс #{index}, #{wagon.type}."
-    end
+    trains[train].wagons.each_with_index { |wagon, index| puts "Вагон #{wagon.type} - (#{index})" }
     print 'Укажите индекс вагона: '
     wagon = gets.chomp.to_i
     print 'Укажите сколько занять места в вагоне: '
-    amount = gets.chomp.to_i
-    trains[train].wagons[wagon].take_place(amount)
+    trains[train].wagons[wagon].take_place(gets.chomp.to_i)
     puts 'Место успешно заполнено!'
   rescue StandardError => e
     puts e.message.to_s
   end
 
-  def show_trains
+  def show_wagons
     show_accessible_trains
     print 'Выберите поезд: '
     choise = gets.chomp.to_i
-    system 'clear'
     block = lambda do |wagon, index|
       puts "Вагон № #{index + 1}, занято #{wagon.class::UNIT}: #{wagon.taken_place}, " \
            "свободно #{wagon.class::UNIT}: #{wagon.free_place}."
@@ -34,25 +30,13 @@ module ManageWagons
     show_accessible_trains
     print 'Введите индекс поезда: '
     train = gets.chomp.to_i
-    if trains[train].type == :cargo && trains[train].speed.zero?
-      print 'Укажите производителя вагона: '
-      company = gets.chomp
-      print 'Укажите вместимость вагона: '
-      volume = gets.chomp.to_i
-      cargo_wagon = CargoWagon.new(volume)
-      trains[train].wagons.push(cargo_wagon)
-      cargo_wagon.manufacturer = company
-      puts "Вагон от производителя '#{cargo_wagon.manufacturer}' успешно присоединен!"
-    elsif trains[train].type == :passenger && trains[train].speed.zero?
-      print 'Укажите производителя вагона: '
-      company = gets.chomp
-      print 'Укажите вместимость вагона: '
-      volume = gets.chomp.to_i
-      passenger_wagon = PassengerWagon.new(volume)
-      trains[train].wagons.push(passenger_wagon)
-      passenger_wagon.manufacturer = company
-      puts "Вагон от производителя '#{passenger_wagon.manufacturer}' успешно присоединен!"
-    end
+    actions = { cargo: CargoWagon, passenger: PassengerWagon }
+    print 'Укажите вместимость вагона: '
+    wagon = actions[trains[train].type].new(gets.chomp.to_i) if trains[train].speed.zero?
+    trains[train].wagons.push(wagon)
+    print 'Укажите производителя вагона: '
+    wagon.manufacturer = gets.chomp
+    puts "Вагон от производителя '#{wagon.manufacturer}' успешно присоединен!"
   end
 
   def unhook_wagon
